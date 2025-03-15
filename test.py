d165eb259d5414692843bdf343538bce805e2fee
soruj-mahmud <sorujmahmudb2h@gmail.com>
2025-03-15 14:30:04 +0600

    sum function add

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 print("Hello World")
 print("Hello World 2")
-print("Hello World 3")
-print("Hello World 4")
+print("Hello World 3 from GitHub")
+print("Hello World 4 from GitHub")
 
 x = 5
 print(x)
+
+def add(a, b):
+    return a + b
+
+print(add(1, 2))
